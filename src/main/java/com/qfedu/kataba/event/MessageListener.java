b9a27a0fe5f125ea72fc45d3fa5de66bb46f5c71
still <stillsmile1992@hotmail.com>
2021-04-12 07:07:13 +0800

    feat:new listener

---
+++ src/main/java/com/qfedu/kataba/event/MessageListener.java
@@ -0,0 +1,12 @@
+package com.qfedu.kataba.event;
+
+/**
+ * @project kataba
+ * @description
+ * @author 徐天麒
+ * @createTime 2021/4/12 7:05
+ * @version 1.0
+ */
+public class MessageListener {
+    //this is a listener
+}
